5b554315916493eca0ad9b091ca603b68f11594b
tanya11029023 <tanya11029023@yahoo.com>
2020-03-15 14:18:09 +0100

    Update DocumentCollectionCell.java

--- DocumentCollectionCell.java
+++ DocumentCollectionCell.java
@@ -1,13 +1,37 @@
 public class DocumentCollectionCell {
-
-private DocumentCollectionCell next;
+    // pointer to the next /previos cell 
+private DocumentCollectionCell next; 
+private DocumentCollectionCell previous; 
+    // document in this cell
 private Document doc;
-
+    //similarity of the document in this cell 
+private double querySimilarity; 
+    
+public DocumentCollectionCell (Document doc, DocumentCollectionCell previous, DocumentCollectionCell next){
+    this.document = document; 
+    this.next = next;
+    this.previous = previous;
+    this.querySimilarity = 0;
+}
+    
 public void setNext (DocumentCollectionCell next){
     this.next = next;
 }
-public void setDoc (Document doc){
-    this.doc = doc;
+    
+public void setPrevious(DocumentCollectionCell previous) {
+    this.previous = previous; 
+}
+    
+public Document setDocument(Document document) {
+    Document oldDoc = this.document;
+    this.document = document;
+    return oldDoc; 
+}
+public void setQuerySimilarity(double querySimilarity) {
+    this.querySimilarity = querySimilarity; 
+}
+public DocumentCollectionCell getPrevious() { 
+    return previous;
 }
 public DocumentCollectionCell getNext (){
     return next;
@@ -15,9 +39,8 @@ public DocumentCollectionCell getNext (){
 public Document getDoc () {
     return doc;
 }
-public DocumentCollectionCell (Document doc){
-this.next = null;
-this.doc = doc;
+public double getQuerySimilarity(){
+    return querySimilarity; 
 }
 
 }
